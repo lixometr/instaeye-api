fdfb5ff1124003b5b45b6aacd81f064abc9eb5e5
lixometr <letsinstudio@gmail.com>
2021-11-20 16:44:31 +0200

    fix env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
